234cabc4d033051ca53cbe4525354521140052fe
DIEGO Araujo <diegoishigh@gmail.com>
2018-11-13 20:35:37 -0300

    Add models

---
+++ api/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.hash(user.password, 12, (err, hash) => {
+        if (err) {
+            return next(err);
+        }
+
+        user.password = hash;
+
+        next();
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
