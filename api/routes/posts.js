73cd7c6ac581667f412f32dc4839bf0e37c79f4e
DIEGO Araujo <diegoishigh@gmail.com>
2019-07-25 20:09:08 -0300

    Add comments to routes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const postsController = require('../controllers/posts');
 const auth = require('../middleware/auth');
 
-router.get('/', postsController.index);
-router.post('/', auth, postsController.store);
-router.get('/seed', auth, postsController.seed);
-router.get('/:id', postsController.show);
-router.patch('/:id', auth, postsController.update);
-router.delete('/:id', auth, postsController.destroy);
+router.get('/', postsController.index); // Retrieve all posts
+router.post('/', auth, postsController.store); // Create a new post
+router.get('/seed', postsController.seed); // Seed posts
+router.get('/:id', postsController.show); // Retrieve a post by its ID
+router.patch('/:id', auth, postsController.update); // Update properties of a post by its ID
+router.delete('/:id', auth, postsController.destroy); // Delete a post by its ID
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 const usersController = require('../controllers/users');
 const auth = require('../middleware/auth');
 
-router.get('/', usersController.index);
-router.post('/', usersController.store);
-router.get('/seed', auth, usersController.seed);
-router.get('/:id', auth, usersController.show);
-router.delete('/:id', auth, usersController.destroy);
+router.get('/', usersController.index); // Retrieve all users
+router.post('/', usersController.store); // Create a new user
+router.get('/seed', usersController.seed); // Seed users
+router.get('/:id', auth, usersController.show); // Retrive a user by its ID
+router.delete('/:id', auth, usersController.destroy); // Delete a user by its ID
 
 module.exports = router;
